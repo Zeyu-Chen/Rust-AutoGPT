8354761f45926513c9cdc0efab3766ca4ee4ed7f
Zeyu Chen <iszeyu.chen@gmail.com>
2024-09-21 22:50:39 +1000

    feat(apis): openai call full feature

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -1,54 +1,74 @@
-use crate::models::general::llm::{ChatCompletion, Message};
+use crate::models::general::llm::{APIResponse, ChatCompletion, Message};
 use dotenv::dotenv;
+use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::Client;
 use std::env;
 
-use reqwest::header::{HeaderMap, HeaderValue};
-
-// Call large Language Model
-pub async fn call_gpt(messages: Vec<Message>) {
+// Call Large Language Model
+pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error + Send>> {
     dotenv().ok();
 
     // Extract API Key information
-    let api_key = env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found in .env");
-    let api_org = env::var("OPEN_AI_ORG").expect("OPEN_AI_ORG not found in .env");
+    let api_key: String =
+        env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found in environment variables");
+    let api_org: String =
+        env::var("OPEN_AI_ORG").expect("OPEN_AI_ORG not found in environment variables");
 
     // Confirm endpoint
-    let url = "https://api.openai.com/v1/chat/completions";
+    let url: &str = "https://api.openai.com/v1/chat/completions";
 
-    // Create header
-    let mut headers = HeaderMap::new();
+    // Create headers
+    let mut headers: HeaderMap = HeaderMap::new();
 
-    // Create OpenAI key header
+    // Create OpenAI Api key header
     headers.insert(
         "authorization",
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap(),
+        HeaderValue::from_str(&format!("Bearer {}", api_key))
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?,
     );
 
     // Create OpenAI Org header
     headers.insert(
         "OpenAI-Organization",
-        HeaderValue::from_str(api_org.as_str()).unwrap(),
+        HeaderValue::from_str(api_org.as_str())
+            .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?,
     );
 
-    // Create Client
-    let client = Client::builder().default_headers(headers).build().unwrap();
+    // Create client
+    let client: Client = Client::builder()
+        .default_headers(headers)
+        .build()
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
 
-    // Create ChatCompletion
-    let chat_completion = ChatCompletion {
+    // Create chat completion
+    let chat_completion: ChatCompletion = ChatCompletion {
         model: "gpt-4o".to_string(),
         messages,
         temperature: 0.1,
     };
 
-    // Troubleshooting
-    let res_raw = client
+    // // Troubleshooting
+    // let res_raw = client
+    //   .post(url)
+    //   .json(&chat_completion)
+    //   .send()
+    //   .await
+    //   .unwrap();
+    // dbg!(res_raw.text().await.unwrap());
+
+    // Extract API Response
+    let res: APIResponse = client
         .post(url)
         .json(&chat_completion)
         .send()
         .await
-        .unwrap();
-    dbg!(res_raw.text().await.unwrap());
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?
+        .json()
+        .await
+        .map_err(|e| -> Box<dyn std::error::Error + Send> { Box::new(e) })?;
+
+    // Send Response
+    Ok(res.choices[0].message.content.clone())
 }
 
 #[cfg(test)]
@@ -64,6 +84,15 @@ mod tests {
 
         let messages: Vec<Message> = vec![message];
 
-        call_gpt(messages).await;
+        let res: Result<String, Box<dyn std::error::Error + Send>> = call_gpt(messages).await;
+        match res {
+            Ok(res_str) => {
+                dbg!(res_str);
+                assert!(true);
+            }
+            Err(_) => {
+                assert!(false);
+            }
+        }
     }
 }
--- src/models/general/llm.rs
+++ src/models/general/llm.rs
@@ -1,4 +1,4 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Clone)]
 pub struct Message {
@@ -11,4 +11,19 @@ pub struct ChatCompletion {
     pub model: String,
     pub messages: Vec<Message>,
     pub temperature: f32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct APIMessage {
+    pub content: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct APIChoice {
+    pub message: APIMessage,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct APIResponse {
+    pub choices: Vec<APIChoice>,
 }
